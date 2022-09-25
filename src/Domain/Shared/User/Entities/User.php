697a2e83fc0a682e5d80c01c54f7113a40873e14
n4m-ward <gabrielphp00@gmail.com>
2022-09-25 18:14:46 -0300

    creating user domain

---
+++ src/Domain/Shared/User/Entities/User.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Newsletter\Domain\Shared\User\Entities;
+
+class User
+{
+    public function __construct(
+        public readonly string $id,
+        public readonly string $name,
+        public readonly Email $email
+    ) {}
+
+    public static function withNameAndEmail(string $name, Email $email): self {
+        return new self(id: uniqid(), name: $name, email: $email);
+    }
+
+    public function updateEmail(Email $email): self
+    {
+        return new self($this->id, $this->name, $email);
+    }
+
+    public function updateUsername(string $username): self
+    {
+        return new self($this->id, $username, $this->email);
+    }
+}
